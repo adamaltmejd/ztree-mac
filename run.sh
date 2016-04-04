b5598b8958026ba56ac7ea709063055496199c8b
Adam Altmejd <adam@altmejd.se>
2016-04-04 17:08:59 +0200

    dont save files to higher directories

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ killgroup(){
 if [[ -z $(ps -ef | grep "ztree" | grep -v grep) ]];
 then
 	# wine ztree.exe /dir datafiles /language en > /dev/null 2>&1 &
-	wine ztree.exe /dir ../datafiles /language en > ../ztree.log 2>&1 &
+	wine ztree.exe /dir datafiles /language en > ztree.log 2>&1 &
 	echo "No ztree.exe running so I'll start it for you. Run the shell script again to start the leaves. To quit ztree.exe use file->quit."
 	exit;
 fi
@@ -35,7 +35,7 @@ then
 	# Start new leaves
 	for i in `seq 1 $leafs`; do
 		# Windowed mode does not seem to work...
-		wine explorer /desktop=$i,800x600 "zleaf.exe" /name "Client $i" /language en >> ../zleaf.log 2>&1 &
+		wine explorer /desktop=$i,800x600 "zleaf.exe" /name "Client $i" /language en >> zleaf.log 2>&1 &
 	done
 	wait
 else
